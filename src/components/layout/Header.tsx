0e709360fb37269779c3cbcfb6148ac79b13b3aa
mauro-sousa <maurodesousa94@gmail.com>
2025-09-22 14:31:54 +0100

    contato

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
-import {Image, Menu, X} from 'lucide-react';
+import { Menu, X} from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 
